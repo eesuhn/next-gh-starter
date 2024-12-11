ad6e13450eeb7602bbd2f6b81896dd4d486acbac
eesuhn <eason.yihong@gmail.com>
2024-12-11 20:52:53 +0800

    Fixed dev opt

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from 'next';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const baseUrl = isProduction ? '/next-starter' : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/next-starter',
+  reactStrictMode: true,
+  trailingSlash: true,
+
+  // I believe these are needed for GitHub Pages
+  basePath: baseUrl,
+  assetPrefix: baseUrl,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
